c9f8885db387e7183ac0858b1a40082cf84afa59
Washington Portugal Gonçalves de Souza Júnior <washington.junior@bs2.com>
2018-10-16 12:50:45 -0300

    node

---
+++ cursoAngularLoiane/hello-typescript/main.js
@@ -0,0 +1,15 @@
+var pessoa = 'washington';
+var feliz = 'feliz';
+var pi = 3.14;
+var arr = [1, 2, 3];
+arr.map(function (v) { return v * 2; });
+var Matematica = /** @class */ (function () {
+    function Matematica() {
+    }
+    Matematica.prototype.sum = function (x, y) {
+        return x + y;
+    };
+    return Matematica;
+}());
+var i = 'a';
+i = 2;
---
+++ cursoAngularLoiane/hello-typescript/main.ts
@@ -0,0 +1,17 @@
+var pessoa = 'washington';
+
+let feliz = 'feliz';
+const pi = 3.14;
+
+let arr = [1,2,3];
+
+arr.map(v => v * 2);
+
+class Matematica {
+    sum(x, y){
+        return x + y;
+    }
+}
+
+var i :string = 'a';
+i = '2';
